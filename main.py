9a8715cd865abcbcc6e772303a9292ed33c5b1be
ruben33206-png <ruben33206@esfundao.pt>
2026-02-10 10:00:20 +0000

    Teste1

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app= FastAPI()
 @app.get("/")
 def read_root():
     return {"mensagem": "API está a funcionar!"}
+
+
+@app.get("/teste")
+def teste_endpoint():
+    Teste = "Dado 1"
+    return {"Teste": Teste}
