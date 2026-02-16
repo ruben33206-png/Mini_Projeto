192d24d46d8d13132aa0d72c4e239e1443a32850
ruben33206-png <ruben33206@esfundao.pt>
2026-02-16 15:07:07 +0000

    TaskHelper_APIfalha

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
-app= FastAPI()
+
+app = FastAPI()
+
 
 @app.get("/")
-def read_root():
-    return {"mensagem": "API está a funcionar!"}
-
-
-@app.get("/teste")
-def teste_endpoint():
-    Teste = 1
-    return {"Teste": Teste}
+def root():
+    return {"message": "API ligada!"}
