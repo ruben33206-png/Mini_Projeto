d39d11aab2c8097d9cd7b1674ec252685662e599
ruben33206-png <ruben33206@esfundao.pt>
2026-02-18 11:14:06 +0000

    TaksHelper_API6.0

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Banco SQLite simples (arquivo local)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./taskhelper.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
--- models.py
+++ models.py
@@ -1,10 +1,24 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
+import json
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
- 
+    id = Column(Integer, primary_key=True)
+    userid = Column(String, unique=True)
+    username = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+    currentlvl = Column(Integer)
+    currentxp = Column(Integer)
+
+class Quest(Base):
+    __tablename__ = "quest"
+
+    id = Column(Integer, primary_key=True)
+    questid = Column(Integer)
+    questname = Column(String)
+    questdescription = -Column(String)
+    requirements = Column(String)
+    howtodoit = Column(String)
